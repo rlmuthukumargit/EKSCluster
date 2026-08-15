95f734532a4ad20a6d9ed133cb72648f03e217c8
Muthukumar Ramu <rl.muthukumar@gmail.com>
2026-08-16 00:45:13 +0530

    Add VPC module configuration using terraform-aws-modules

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -6,3 +6,23 @@ terraform {
     }
   }
 }
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+
+  azs             = var.azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = var.enable_nat_gateway
+  single_nat_gateway = var.single_nat_gateway
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = var.tags
+}
