096ca654f755bbc1b15e252eca4e9f1ecc77b5f4
Muthukumar Ramu <rl.muthukumar@gmail.com>
2026-08-16 00:43:42 +0530

    Remove unsupported manage_aws_auth argument

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -16,9 +16,7 @@ module "eks" {
   vpc_id          = var.vpc_id
   subnet_ids      = var.private_subnets
 
-  enable_irsa     = true
-  manage_aws_auth = true
-
+  enable_irsa             = true
   eks_managed_node_groups = var.node_groups
 
   tags = var.tags
