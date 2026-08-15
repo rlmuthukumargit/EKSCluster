7a8b3913d671269c61e1f17abbcdb8970093c4cc
Muthukumar Ramu <rl.muthukumar@gmail.com>
2026-08-16 00:43:13 +0530

    Fix EKS module input variable names for v19.x compatibility

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -9,17 +9,17 @@ terraform {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = ">= 19.0.0"
+  version = "~> 19.0"
 
   cluster_name    = var.cluster_name
   cluster_version = var.cluster_version
   vpc_id          = var.vpc_id
-  subnets         = var.private_subnets
+  subnet_ids      = var.private_subnets
 
-  enable_irsa      = true
-  manage_aws_auth  = true
+  enable_irsa     = true
+  manage_aws_auth = true
 
-  node_groups = var.node_groups
+  eks_managed_node_groups = var.node_groups
 
   tags = var.tags
 }
